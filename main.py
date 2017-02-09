a804521ccca1093848b856fd4873026c79285d77
Andrew Dabski <jedrus1133@gmail.com>
2017-02-09 03:03:00 +0100

    try to pickle

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from logic_ai import AI
 
-print ("start")
-for x in range(0,10):
-    AI(x)
+print ("loading brain structure..")
+filename = 'brain_ai.txt'
+file = open(filename,'r')
+dataPickle = file.read()
+file.close()
+
+self.__dict__ = cPickle.loads(dataPickle)
+brain = AI()
+
+print ("start learning")
+for x in range(0,100):
+
+
+file = open(filename,'w')
+file.write(cPickle.dumps(self.__dict__))
+file.close()
