fd61706c7cf650bfd55ea2eb81d26b7532f40973
andrew <jedrus1133@gmail.com>
2017-02-09 03:39:28 +0100

    actually save and load brain

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import class_loader
 from logic_ai import AI
 
 print ("loading brain structure..")
 
-brain = AI()
+brain = class_loader.load(AI)
 
 print ("start learning")
 for x in range(0,100):
+    pass
+
+class_loader.save(brain)
